d9b1c841c3fab237a8021cb4ff6aa4ff646d1333
Malik16052 <malikea-bpa202@code.edu.az>
2025-11-17 04:39:40 +0400

    Program class-i

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Login_Bank_Account;
+using static Login_Bank_Account.AccountS;
+using static Login_Bank_Account.BankS;
+using static Login_Bank_Account.MenuS;
+
+class Program
+{
+    static void Main()
+    {
+        Bank bank = new Bank { Id = 1 };
+        BankService bankService = new BankService(bank);
+        AccountService accountService = new AccountService(bankService);
+        MenuService menuService = new MenuService(accountService, bankService);
+
+        menuService.Start();
+    }
+}
