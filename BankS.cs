f08c5c4a2b43445c0c5741b1c1bbaaa46931339e
Malik16052 <malikea-bpa202@code.edu.az>
2025-11-17 04:39:10 +0400

    Bank Service class-i

---
+++ BankS.cs
@@ -0,0 +1,35 @@
+﻿namespace Login_Bank_Account;
+
+public class BankS
+{
+    public class BankService
+    {
+        private Bank _bank;
+
+        public BankService(Bank bank)
+        {
+            _bank = bank;
+        }
+
+        public void AddUser(User user)
+        {
+            _bank.Users.Add(user);
+        }
+
+        public User FindUser(string email)
+        {
+            return _bank.Users.FirstOrDefault(u => u.Email == email);
+        }
+
+        public bool IsEmailTaken(string email)
+        {
+            return _bank.Users.Any(u => u.Email == email);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return _bank.Users;
+        }
+    }
+
+}
